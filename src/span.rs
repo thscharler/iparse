d509e8479ee03d837ace9277b2cf2cc4c6449526
stommy <thscharler@gmail.com>
2022-12-03 20:40:03 +0100

    Revisit span_union to make its use safe.
    Add asserts to check that both spans originate from the same source, check for underflow.
    
    Should be fine now.

--- src/span.rs
+++ src/span.rs
@@ -11,11 +11,22 @@ use std::str::from_utf8_unchecked;
 /// # Safety
 ///  See span_union for details.
 pub unsafe fn span_union_opt<'a>(span0: Option<Span<'a>>, span1: Span<'a>) -> Span<'a> {
+    match span0 {
+        None => span1,
+        Some(span0) => span_union(span0, span1),
+    }
+}
+
+// from nom_locate-4.0.0
+fn get_unoffsetted_ptr(span0: Span<'_>) -> *const u8 {
+    let self_bytes = span0.fragment().as_bytes();
+    let self_ptr = self_bytes.as_ptr();
     unsafe {
-        match span0 {
-            None => span1,
-            Some(span0) => span_union(span0, span1),
-        }
+        assert!(
+            span0.location_offset() <= isize::MAX as usize,
+            "offset is too big"
+        );
+        self_ptr.offset(-(span0.location_offset() as isize))
     }
 }
 
@@ -24,26 +35,133 @@ pub unsafe fn span_union_opt<'a>(span0: Option<Span<'a>>, span1: Span<'a>) -> Sp
 /// # Safety
 ///
 /// If any of the following conditions are violated, the result is Undefined Behavior:
-/// * Both the starting and other pointer must be either in bounds or one byte past the end of the same allocated object.
-///      Should be guaranteed if both were obtained from on ast run.
+/// * Both the starting and other pointer must be either in bounds or one byte past the end
+///   of the same allocated object.
 /// * Both pointers must be derived from a pointer to the same object.
-///      Should be guaranteed if both were obtained from on ast run.
+///       
+///     Use get_unoffsetted_slice from nom_locate-4.0.0 to compare the original
+///     pointers of both spans.
+///
 /// * The distance between the pointers, in bytes, cannot overflow an isize.
+///     Assert that span0 has a lower offset than span1.
+///
 /// * The distance being in bounds cannot rely on “wrapping around” the address space.
-pub unsafe fn span_union<'a>(span0: Span<'a>, span1: Span<'a>) -> Span<'a> {
-    let ptr = span0.as_ptr();
-    // offset to the start of span1 and add the length of span1.
-    let size = span0.offset(&span1) + span1.len();
+pub fn span_union<'a>(span0: Span<'a>, span1: Span<'a>) -> Span<'a> {
+    // should be a good start.
+    assert_eq!(get_unoffsetted_ptr(span0), get_unoffsetted_ptr(span1));
 
     unsafe {
-        // The size should be within the original allocation, if both spans are from
-        // the same ast run. We must ensure that the ast run doesn't generate
-        // Spans out of nothing that end in the ast.
-        let slice = slice::from_raw_parts(ptr, size);
-        // This is all from a str originally and we never got down to bytes.
+        let self_ptr = span0.fragment().as_ptr();
+
+        // Calculate the relative offset of span1 and add its length.
+        assert!(span0.location_offset() <= span1.location_offset());
+        let new_len = span0.offset(&span1) + span1.len();
+        let slice = slice::from_raw_parts(self_ptr, new_len);
+
+        // span0 was a valid str before so this should be ok.
         let str = from_utf8_unchecked(slice);
 
-        // As span0 was ok the offset used here is ok too.
-        Span::new_from_raw_offset(span0.location_offset(), span0.location_line(), str, ())
+        // Copy everything else from span0
+        Span::new_from_raw_offset(
+            span0.location_offset(),
+            span0.location_line(),
+            str,
+            span0.extra,
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::error::ParserError;
+    use crate::span::span_union;
+    use crate::{Code, ParserNomResult, Span};
+    use nom::bytes::complete::{take_while, take_while1};
+    use nom::character::complete::digit1;
+    use nom::combinator::recognize;
+    use nom::sequence::preceded;
+    use std::fmt::{Debug, Display, Formatter};
+
+    #[test]
+    pub fn test_union_ok() {
+        let span = Span::new("1234 test");
+        let _other = Span::new("5678 xxxx");
+
+        let (rest, number) = nom_number(span).unwrap();
+        let (_rest, name) = nom_name(rest).unwrap();
+
+        span_union(number, name);
+    }
+
+    #[test]
+    #[should_panic]
+    pub fn test_union_order() {
+        let span = Span::new("1234 test");
+        let _other = Span::new("5678 xxxx");
+
+        let (rest, number) = nom_number(span).unwrap();
+        let (_rest, name) = nom_name(rest).unwrap();
+
+        span_union(name, number);
+    }
+
+    #[test]
+    #[should_panic]
+    pub fn test_union_other_1() {
+        let span = Span::new("1234 test");
+        let other = Span::new("5678 xxxx");
+
+        let (rest, _number) = nom_number(span).unwrap();
+        let (_rest, name) = nom_name(rest).unwrap();
+
+        span_union(other, name);
+    }
+
+    #[test]
+    #[should_panic]
+    pub fn test_union_other_2() {
+        let span = Span::new("1234 test");
+        let other = Span::new("5678 xxxx");
+
+        let (rest, _number) = nom_number(span).unwrap();
+        let (_rest, name) = nom_name(rest).unwrap();
+
+        span_union(name, other);
+    }
+
+    #[allow(dead_code)]
+    #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+    enum TCode {
+        Number,
+        Name,
+    }
+
+    impl Display for TCode {
+        fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
+            unreachable!()
+        }
+    }
+
+    impl Code for TCode {
+        const NOM_ERROR: Self = Self::Number;
+        const NOM_FAILURE: Self = Self::Number;
+        const PARSE_INCOMPLETE: Self = Self::Number;
+    }
+
+    fn nom_number(i: Span<'_>) -> ParserNomResult<'_, TCode> {
+        preceded(nom_ws, digit1)(i)
+    }
+
+    fn nom_name(i: Span<'_>) -> ParserNomResult<'_, TCode> {
+        preceded(
+            nom_ws,
+            recognize(take_while1(|c: char| c.is_alphanumeric())),
+        )(i)
+    }
+
+    fn nom_ws<'s>(i: Span<'s>) -> ParserNomResult<'s, TCode> {
+        recognize(take_while::<_, _, ParserError<'s, TCode>>(|c: char| {
+            c == ' ' || c == '\t'
+        }))(i)
     }
 }
